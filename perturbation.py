95767037b110a9b32d829707b5f6eed685e7f6d9
Shivanvitha Ambati <shivanvitha21@gmail.com>
2024-06-19 23:06:57 +0530

    handled R_t_i

--- perturbation.py
+++ perturbation.py
@@ -615,19 +615,19 @@ def main1(_config):
         elif method_name == "raw_attn":
             item['img_id'] = COCO_path + item['img_id']
             R_t_t, R_t_i = demo_vqa_rm.main1(_config, item, model=model_pert, is_pert=True, viz=False, tokenizer=model_pert.tokenizer)
-            # R_t_i = torch.cat((torch.zeros(1).to(device), R_t_i))
+            R_t_i = torch.cat((torch.zeros(1).to(device), R_t_i))
 
 
         elif method_name == "attn_gradcam":
             item['img_id'] = COCO_path + item['img_id']
             R_t_t, R_t_i = demo_vqa_rm.main1(_config, item, model=model_pert, is_pert=True, viz=False, tokenizer=model_pert.tokenizer)
-            # R_t_i = torch.cat((torch.zeros(1).to(device), R_t_i))
+            R_t_i = torch.cat((torch.zeros(1).to(device), R_t_i))
 
     
         elif method_name == "rollout":
             item['img_id'] = COCO_path + item['img_id']
             R_t_t, R_t_i = demo_vqa_rm.main1(_config, item, model=model_pert, is_pert=True, viz=False, tokenizer=model_pert.tokenizer)
-            # R_t_i = torch.cat((torch.zeros(1).to(device), R_t_i))
+            R_t_i = torch.cat((torch.zeros(1).to(device), R_t_i))
 
         # elif method_name == "dsm":
             # R_t_t, R_t_i = ours.generate_ours_dsm(item, use_lrp = False)
